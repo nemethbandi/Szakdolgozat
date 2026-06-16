dbf519b0a79d8415b46306a98991366cfb4ad683
nemethbandi <andras.nemeth1@stud.uni-corvinus.hu>
2026-06-16 10:54:00 +0200

    add new files

--- Kódbázis/src/data_loader.py
+++ Kódbázis/src/data_loader.py
@@ -1,6 +1,15 @@
 import yfinance as yf
 import pandas as pd
 
+# Ez a modul az árfolyamadatok letöltésére és mentésére szolgál. Ehhez a yahoo finance API-t használja.
+
+#load_data függvény letölti az árfolyamadatokat a megadott szimbólumhoz és időintervallumhoz, 
+# majd visszaadja az adatokat egy pandas DataFrame-ben.
+
+#save_data_to_csv függvény elmenti a pandas DataFrame-ben lévő adatokat egy CSV fájlba a megadott fájlnévvel.
+
+#készítette: Németh András
+
 def load_data(symbol, start_date, end_date):
     
     data = yf.download(symbol, start=start_date, end=end_date)
